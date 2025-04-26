bb4d2221328799160f163ddb84b98b3ca5956cda
AndrewsNick-FS <148885746+AndrewsNick-FS@users.noreply.github.com>
2025-04-25 21:28:35 -0500

    Accidentally got rid of all changes, used a backup copy I had made and reverted to where I needed the code to be

--- cardMatchGame/src/ts/game.ts
+++ cardMatchGame/src/ts/game.ts
@@ -1,3 +1,5 @@
+import "../../src/styles/styles.scss";
+
 (() => {
   interface Card {
     element: HTMLElement;
--- cardMatchGame/webpack.config.js
+++ cardMatchGame/webpack.config.js
@@ -1,47 +1,48 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/ts/game.ts",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      "@": path.resolve(__dirname, "src"),
     },
-    extensions: ['.js', '.scss', '.css'],
+    extensions: [".ts", ".tsx", ".js", ".scss", ".css"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Test for .js files
+        test: /\.ts$/, // Test for .js files
+        use: "ts-loader",
         exclude: /node_modules/, // Exclude node_modules
         use: {
-          loader: 'babel-loader', // Use Babel to transpile ES6 to ES5
+          loader: "babel-loader", // Use Babel to transpile ES6 to ES5
           options: {
-            presets: ['@babel/preset-env'], // Babel preset for ES6+ features
+            presets: ["@babel/preset-env"], // Babel preset for ES6+ features
           },
         },
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   devServer: {
     compress: false,
     port: 9000,
     hot: true,
-    static: './build',
-    open: true
+    static: "./build",
+    open: true,
   },
-  mode: 'development'
+  mode: "development",
 };
